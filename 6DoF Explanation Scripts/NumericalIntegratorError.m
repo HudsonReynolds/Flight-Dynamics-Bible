f25f7700a66f09817d45f272a4c8ba42bc5ece00
HudsonReynolds <73318449+HudsonReynolds@users.noreply.github.com>
2025-03-06 22:20:10 -0500

    script push

--- 6DoF Explanation Scripts/NumericalIntegratorError.m
+++ 6DoF Explanation Scripts/NumericalIntegratorError.m
@@ -1,37 +1,44 @@
 % Numerical Integrator Error Showcase:
 close all; clear; clc;
 
-% Describe our initial ODE:
-
-% describe the state vector, Y:
-
 % initial condition, y(0) = 0
-Y = 0;
+y = 0;
+yRK2 = y;
+yRK4 = y;
 
 % timespan over which to integrate:
 dt = 0.5;
 tEnd = 3;
 t = 0:dt:tEnd;
 
-% numerically integrate with explicit euler
-for i=1:length(t)
-    Y(i+1) = Y(i) + YDot(t(i),Y) * dt;
+%integration loop:
+for i=1:length(t)-1
+    % numerically integrate with explicit euler
+    y(i+1) = y(i) + YDot(t(i),y(i)) * dt;
+    % numerically integrate with RK2
+    yRK2(i+1) = rk2(@(t,y)YDot(t(i),y), dt, t, yRK2(i));
+    % numerically integrate with RK4
+    yRK4(i+1) = rk4(@(t,y)YDot(t(i),y), dt, t, yRK4(i));
 end
 
-tspan = linspace(0,tEnd, 100);
+% tspan for the original function:
+tspanCurve = linspace(0,tEnd, 100);
 
+%figure plotting:
 hfig = figure;  % save the figure handle in a variable
-fname = 'Explicit Euler Approximation Figure';
+fname = 'Numerical Integrator Comparison Figure';
 
 picturewidth = 20; % set this parameter and keep it forever
 hw_ratio = 0.75; % feel free to play with this ratio
 set(findall(hfig,'-property','FontSize'),'FontSize',18) % adjust fontsize to your document
 
-plot(t, Y(1:end-1), '.', LineStyle='-', MarkerSize= 12)
+plot(t, y, '.', LineStyle='-', MarkerSize= 12)
 hold on
-plot(tspan, tspan.^2)
+plot(t, yRK2, '.', LineStyle='-', MarkerSize= 12)
+plot(t, yRK4, '.', LineStyle='-', MarkerSize= 12)
+plot(tspanCurve, tspanCurve.^2, LineWidth=1)
 
-legend('Numerical Approximation', '$y=t^2$', 'Location', 'northwest')
+legend('Explicit Euler', 'RK2', 'RK4', '$y=t^2$', 'Location', 'northwest')
 
 %title('Explicit Euler Numerical Approximation')
 xlabel('$t$')
@@ -48,6 +55,28 @@ pos = get(hfig,'Position');
 set(hfig,'PaperPositionMode','Auto','PaperUnits','centimeters','PaperSize',[pos(3), pos(4)])
 print(hfig,fname,'-dpng','-r400')
 
+
+% functions
+
+%euler
 function[out] = YDot(t,Y)
 out = 2 * t;
+end
+
+%rk2
+function out = rk2(fun, dt, tIn, xIn)
+    f1 = fun(tIn,xIn);
+    f2 = fun(tIn + dt/2, xIn + dt .* f1);
+    
+    out = xIn + (dt / 2)*(f1 + f2);
+end
+
+%rk4
+function out = rk4(fun, dt, tIn, xIn)
+    f1 = fun(tIn,xIn);
+    f2 = fun(tIn + dt/2, xIn + (dt/2) .* f1);
+    f3 = fun(tIn + dt/2, xIn + (dt/2) .* f2);
+    f4 = fun(tIn + dt, xIn + dt*f3);
+    
+    out = xIn + (dt / 6)*(f1 + 2*f2 + 2*f3+f4);
 end
